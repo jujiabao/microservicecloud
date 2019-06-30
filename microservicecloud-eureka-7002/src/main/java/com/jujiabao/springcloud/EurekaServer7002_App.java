e8410e33acf469095bea4156f476fc1dee38c35c
jujiabao <694455391@qq.com>
2019-06-30 19:40:16 +0800

    集群eureka配置使用样例，主要其中在yml中，多个服务注册于集群模拟

---
+++ microservicecloud-eureka-7002/src/main/java/com/jujiabao/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,21 @@
+package com.jujiabao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Title EurekaServer7001_App
+ * @ProjectName microservicecloud
+ * @Description 描述一下吧
+ * @Author Hello.Ju
+ * @Date 2019-06-27 22:44
+ */
+@SpringBootApplication
+@EnableEurekaServer//表示该boot为eureka服务，接受其它的微服务注册进来
+public class EurekaServer7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
