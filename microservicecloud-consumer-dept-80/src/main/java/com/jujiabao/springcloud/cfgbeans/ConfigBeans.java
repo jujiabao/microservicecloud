1ced8909af1782b783e4c61558c1dbfa4c4d4f30
jujiabao <694455391@qq.com>
2019-07-01 23:19:09 +0800

    ribbon负载均衡修改为随机负载均衡算法样例

--- microservicecloud-consumer-dept-80/src/main/java/com/jujiabao/springcloud/cfgbeans/ConfigBeans.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/jujiabao/springcloud/cfgbeans/ConfigBeans.java
@@ -1,5 +1,8 @@
 package com.jujiabao.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +26,12 @@ public class ConfigBeans {
     @LoadBalanced//开启负载均衡-ribbon，基于客户端的负载均衡
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule myRule(){
+        //return new RoundRobinRule();//默认，轮询负载均衡
+        return new RandomRule();//轮询负载均衡算法
     }
 
 }
