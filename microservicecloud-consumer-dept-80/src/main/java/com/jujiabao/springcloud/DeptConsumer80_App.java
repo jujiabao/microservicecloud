cc23cadaf8f57e4e0cdbceba205558b05ff89ceb
jujiabao <694455391@qq.com>
2019-07-02 23:29:30 +0800

    自定义ribbon负载均衡算法样例，注意@RibbonClient使用规范

--- microservicecloud-consumer-dept-80/src/main/java/com/jujiabao/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/jujiabao/springcloud/DeptConsumer80_App.java
@@ -1,9 +1,11 @@
 package com.jujiabao.springcloud;
 
+import com.jujiabao.rules.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @Title DeptConsumer80_App
@@ -15,6 +17,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
+//在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效
+//@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration= MySelfRule.class)
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
